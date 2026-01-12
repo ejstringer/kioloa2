1a7842f1e7f71e2e62c66ce3d14cd499a968d0f8
Emily Stringer <ejstringer@live.com.au>
2026-01-12 16:09:21 +1100

    trial setup

---
+++ R/welcome.R
@@ -0,0 +1,9 @@
+# Hello and welcome to the dartR workshop!
+#
+# This is an example function named 'welcome'
+# which prints 'Hello, world!'.
+#
+
+welcome <- function() {
+  print("Welcome to the dartR workshop")
+}
